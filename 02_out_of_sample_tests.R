9177882edcd23332f252a2191aa35a446b595f81
ctruciosm <ctrucios@gmail.com>
2024-07-26 08:57:55 -0300

    up

--- 02_out_of_sample_tests.R
+++ 02_out_of_sample_tests.R
@@ -113,12 +113,75 @@ for (i in 2:46) {
 }
 
 
+# lambda 05
+Ropt_ls_0 <- read.csv("Results/Rport_05_1_120_ls_0_ibrx.csv")[, c(2:10, 1)]
+Ropt_ls_1 <- read.csv("Results/Rport_05_1_120_ls_1_ibrx.csv")[, c(2:10, 1)]
+Ropt_ls_2 <- read.csv("Results/Rport_05_1_120_ls_2_ibrx.csv")[, c(2:10, 1)]
+Ropt_ls_3 <- read.csv("Results/Rport_05_1_120_ls_3_ibrx.csv")[, c(2:10, 1)]
+Ropt_ls_4 <- read.csv("Results/Rport_05_1_120_ls_4_ibrx.csv")[, c(2:10, 1)]
+
+# Tests within panel (panel = cov estimator)
+p_values_05 <- matrix(NA, ncol = 5, nrow = 9)
+for (i in 2:10) {
+  set.seed(i)
+  p_values_05[i - 1, 1] <- boot.time.inference(Ropt_ls_0[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_05[i - 1, 2] <- boot.time.inference(Ropt_ls_1[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_05[i - 1, 3] <- boot.time.inference(Ropt_ls_2[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_05[i - 1, 4] <- boot.time.inference(Ropt_ls_3[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_05[i - 1, 5] <- boot.time.inference(Ropt_ls_4[, c(1, i)], b = block, M = 500)$p.Value
+}
+rownames(p_values_05) <- colnames(Ropt_ls_0)[-1]
+
+
+Ropt_mv <- cbind(Ropt_ls_0[, 1:9], Ropt_ls_1[, 1:9], Ropt_ls_2[, 1:9], Ropt_ls_3[, 1:9], Ropt_ls_4)
+p_values_05_full <- c()
+for (i in 2:46) {
+  set.seed(i)
+  p_values_05_full[i - 1] <- boot.time.inference.log.var(Ropt_mv[, c(1, i)], b = block, M = 500)$p.Value
+}
+
+
+# lambda 11
+Ropt_ls_0 <- read.csv("Results/Rport_05_1_120_ls_0_ibrx.csv")[, c(12:20, 11)]
+Ropt_ls_1 <- read.csv("Results/Rport_05_1_120_ls_1_ibrx.csv")[, c(12:20, 11)]
+Ropt_ls_2 <- read.csv("Results/Rport_05_1_120_ls_2_ibrx.csv")[, c(12:20, 11)]
+Ropt_ls_3 <- read.csv("Results/Rport_05_1_120_ls_3_ibrx.csv")[, c(12:20, 11)]
+Ropt_ls_4 <- read.csv("Results/Rport_05_1_120_ls_4_ibrx.csv")[, c(12:20, 11)]
+
+# Tests within panel (panel = cov estimator)
+p_values_1 <- matrix(NA, ncol = 5, nrow = 9)
+for (i in 2:10) {
+  set.seed(i)
+  p_values_1[i - 1, 1] <- boot.time.inference(Ropt_ls_0[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_1[i - 1, 2] <- boot.time.inference(Ropt_ls_1[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_1[i - 1, 3] <- boot.time.inference(Ropt_ls_2[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_1[i - 1, 4] <- boot.time.inference(Ropt_ls_3[, c(1, i)], b = block, M = 500)$p.Value
+  p_values_1[i - 1, 5] <- boot.time.inference(Ropt_ls_4[, c(1, i)], b = block, M = 500)$p.Value
+}
+rownames(p_values_1) <- colnames(Ropt_ls_0)[-1]
+
+
+Ropt_mv <- cbind(Ropt_ls_0[, 1:9], Ropt_ls_1[, 1:9], Ropt_ls_2[, 1:9], Ropt_ls_3[, 1:9], Ropt_ls_4)
+p_values_1_full <- c()
+for (i in 2:46) {
+  set.seed(i)
+  p_values_1_full[i - 1] <- boot.time.inference.log.var(Ropt_mv[, c(1, i)], b = block, M = 500)$p.Value
+}
+
+
+
+
+
 p_values_mv
 p_values_2
 p_values_5
 p_values_7
+p_values_05
+p_values_1
 
 p_values_mv_full
 p_values_2_full
 p_values_5_full
 p_values_7_full
+p_values_05_full
+p_values_1_full
