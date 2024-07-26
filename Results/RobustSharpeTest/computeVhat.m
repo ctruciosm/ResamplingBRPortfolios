44e4fbe37b4c668a674cf0194a534ae0ec175705
ctruciosm <ctrucios@gmail.com>
2024-07-26 09:07:09 -0300

    up

--- Results/RobustSharpeTest/computeVhat.m
+++
@@ -1,6 +0,0 @@
-function [vhat]=computeVhat(X)
-
-nu = [mean(X(:,1)); mean(X(:,2)); mean(X(:,1).^2); mean(X(:,2).^2)];
-nux = [X(:,1)-nu(1) X(:,2)-nu(2) X(:,1).^2-nu(3) X(:,2).^2-nu(4)];
-vhat=nux;
-end
--- Results/RobustSharpeTest/regnoint.m
+++
@@ -1,4 +0,0 @@
-function [coef,resi]=regnoint(X,y)
-coef=inv(X'*X)*X'*y;
-resi=y-X*coef;
-end
